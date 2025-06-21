52751a0fd416bf31d4a2fa8c6bbf755397089809
Eddie Rosas <erosas9172@sdsu.edu>
2025-06-21 16:31:25 -0700

    Update mainwildfire.js

--- assets/maps/mainwildfire.js
+++ assets/maps/mainwildfire.js
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>LA Wildfire Map</title>
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
-  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-  <style>
-    #map { height: 100vh; }
-  </style>
-</head>
-<body>
-  <div id="map">Loading map...</div>
-  <script src="mainwildfire.js"></script>
-</body>
-</html>
-
 const LACountyWildfireData = {
     hazardZones: [
         {
